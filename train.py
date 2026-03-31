70476c90eed4434d8a9b306e33af7ec65346240e
shashwat-srivastava07 <shashwat.25bce11121@vitbhopal.ac.in>
2026-03-31 15:49:22 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
-print("Training started...")
-
+print("Training started!")
 import pandas as pd
 import os
 import pickle
@@ -16,31 +15,24 @@ def clean_text(text):
     text = re.sub(r'[^a-zA-Z\s]', '', text)
     text = re.sub(r'\s+', ' ', text).strip()
     return text
-
 data = pd.read_csv("dataset.csv")
-
 data["text"] = data["text"].apply(clean_text)
 
 X = data["text"]
 y = data["emotion"]
-
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42, stratify=y
-)
+    X, y, test_size=0.2, random_state=42, stratify=y)
 
 model = Pipeline([
     ("tfidf", TfidfVectorizer(stop_words='english', ngram_range=(1,2))),
-    ("classifier", LogisticRegression(max_iter=1000))
-])
+    ("classifier", LogisticRegression(max_iter=1000))])
 
-model.fit(X_train, y_train)
-
+model.fit(X_train, y_train
 y_pred = model.predict(X_test)
 
 print("\nModel Accuracy:", accuracy_score(y_test, y_pred))
 print("\nClassification Report:\n")
 print(classification_report(y_test, y_pred))
-
 print("\nConfusion Matrix:\n")
 print(confusion_matrix(y_test, y_pred))
 
@@ -48,5 +40,4 @@ os.makedirs("model", exist_ok=True)
 
 with open("model/emotion_model.pkl", "wb") as f:
     pickle.dump(model, f)
-
 print("\nModel saved successfully in model/emotion_model.pkl")
